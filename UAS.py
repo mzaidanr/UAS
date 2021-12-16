e527eee38229967ed913ad2a164ec23d80b94e7b
mzaidanr <96235649+mzaidanr@users.noreply.github.com>
2021-12-17 06:40:31 +0700

    Update UAS.py

--- UAS.py
+++ UAS.py
@@ -1,165 +1,103 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 import streamlit as st
 from fileHandler import csvHandler,jsonHandler
-import numpy as np
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-import plotly.graph_objects as go
-import plotly.express as px
-import matplotlib.pyplot as plt
 
-
-st.title('Tubes Prokom')
-st.header('Produksi Minyak Mentah')
-ch_ = csvHandler('produksi_minyak_mentah.csv')
 jh_ = jsonHandler('kode_negara_lengkap.json')
+dfJ = jh_.dataFrame
+ch_ = csvHandler('produksi_minyak_mentah.csv')
+dfC = ch_.dataFrame
 
-#--Poin (a)--
-df_ = ch_.dataFrame
-df_info = jh_.dataFrame
-negara_li = df_info['name'].tolist()
+#Bagian A
+st.sidebar.title('Pilihan Pengaturan')
+left_col, mid_col, right_col = st.columns(3)
 
-negara = st.selectbox('Pilih negara : ',negara_li) 
+name_negara = dfJ['name'].tolist()
 
+negara = st.sidebar.selectbox('Pilih Negara : ', name_negara)
 
-kode = df_info[df_info['name']==negara]['alpha-3'].tolist()[0]
+kode_negara = dfJ[dfJ['name']==negara]['alpha-3'].tolist()[0] 
+N = kode_negara
+tahun = dfC[dfC['kode_negara']==N]['tahun'].tolist()
+produksi = dfC[dfC['kode_negara']==N]['produksi'].tolist()
 
+dic = {'tahun':tahun,'produksi':produksi}
+df_ = pd.DataFrame(dic)
+dfJ['alpha-3'][243]
 
-st.write('Kode negara : ',kode)
-st.write('Negara : ',negara)
+st.write('Grafik Produksi Minyak Negara', negara)
 
-x_ = df_[df_['kode_negara']==kode]['tahun'].tolist()
-y_ = df_[df_['kode_negara']==kode]['produksi'].tolist()
+plt.title('Produksi Tahunan Negara {}'.format(negara))
+plt.plot(tahun,produksi,label='Nilai Produksi')
+plt.xlabel('Tahun')
+plt.ylabel('Produksi')
+plt.legend()
 
-reg = LinearRegression()
-reg.fit(np.array(x_).reshape(-1,1),np.array(y_))
-m = reg.coef_[0]
-c = reg.intercept_
-y_trend = [m*x+c for x in x_]
-if c >= 0:
-    equation = 'y={m:.2f}x+{c:.2f}'.format(m=m,c=c)
-else:
-    equation = 'y={m:.2f}x{c:.2f}'.format(m=m,c=c)
+#Bagian B
+st.sidebar.write('Pengaturan Produksi Negara per Tahun')
+T = st.sidebar.number_input("Pilih Tahun produksi", min_value=1971, max_value=2015)
+B = st.sidebar.number_input("Pilih Banyak Negara", min_value=1, max_value=None, value=3)
 
-dic = {'tahun':x_,'produksi':y_}
-st.write(pd.DataFrame(dic))
+dfC = dfC[dfC['tahun']==T]
+kode_negara = dfC[dfC['tahun']==T]['kode_negara'].tolist()
+produksi = dfC[dfC['tahun']==T]['produksi'].tolist()
 
-plotting = st.selectbox('Pilih tipe plotting : ',['tipe 1','tipe 2'])
-
-if plotting == 'tipe 1':
-    plt.title('Data Produksi {}'.format(negara))
-    plt.plot(x_,y_,label='Actual')
-    plt.plot(x_,y_trend,label='Trendline\n{}'.format(equation))
-    plt.xlabel('Tahun')
-    plt.ylabel('Produksi')
-    plt.legend()
-    st.pyplot(plt)
-else:
-    dic['trendline'] = y_trend
-    fig = px.scatter(pd.DataFrame(dic),x='tahun',y='produksi',trendline='lowess',trendline_options=dict(frac=0.1))
-    st.plotly_chart(fig)
-
-#--Poin (b)--
-st.write()
-st.write()
-st.header('Bagian B COKK')
-
-
-B = st.sidebar.number_input("Berapa besar negara?", min_value=1, max_value=None)
-T = st.sidebar.number_input("Tahun produksi", min_value=1971, max_value=2015)
-
-df = df_
-dfJ = df_info
-
-df = df[df['tahun']==T]
-kode_negara = df[df['tahun']==T]['kode_negara'].tolist()
-# produksi = df[df['tahun']==T]['produksi'].tolist()
-
-produksi_maks = []
-negara_pertahun = []
+maks_produksi = []
+negara_tiaptahun = []
 
 kode_negara = list(dict.fromkeys(kode_negara))
 for kode in kode_negara:
     try:
-        produksi = df[df['kode_negara']==kode]['produksi'].tolist()
+        produksi = dfC[dfC['kode_negara']==kode]['produksi'].tolist()
         negara = dfJ[dfJ['alpha-3']==kode]['name'].tolist()[0]
-        produksi_maks.append(max(produksi))
-        negara_pertahun.append(negara)
+        maks_produksi.append(max(produksi))
+        negara_tiaptahun.append(negara)
     except:
         continue
         
-dic = {'negara':negara_pertahun,'produksi_maks':produksi_maks}
+dic = {'negara':negara_tiaptahun,'produksi_maks':maks_produksi}
 df__ = pd.DataFrame(dic)
 df__ = df__.sort_values('produksi_maks',ascending=False).reset_index()
 
-plt.clf() # clear the figure
-
-#tulisan nanti lu aja ya, gua update ke github dulu
-
 plt.title('{B} Negara dengan Produksi Terbesar pada Tahun {T}'.format(B=B,T=T))
-plt.bar(df__['negara'][:B],df__['produksi_maks'][:B],width=0.9, bottom=None, align="center",
+plt.bar(df__['negara'][:B],df__['produksi_maks'][:B],width=0.5, bottom=None, align="center",
             color="lightblue", edgecolor="aquamarine", data=None, zorder=3)
-plt.grid(True, color="grey", linewidth="0.7", linestyle="-.", zorder=0)
-plt.xlabel('negara')
-plt.ylabel('produksi_maksimum')
+plt.grid(True, color="blue", linewidth="0.7", linestyle="-.", zorder=0)
+plt.xlabel('Negara')
+plt.ylabel('Banyaknya Produksi')
+plt.show()
 
-st.write('Input banyak negara dan tahun di kiri')
-st.pyplot(plt)
+#Bagian C
 
-#--Poin (c)--
-st.write()
-st.write()
-st.header('Bagian C COKK')
+st.write('Grafik Negara dengan Produksi Kumulatif Terbesar')
+st.sidebar.write('Pengaturan Produksi Negara per Tahun')
+B = st.sidebar.number_input("Pilih Banyak Negara", min_value=1, max_value=None)
 
+list_a = []
+kumulatif = []
 
-B_ = st.sidebar.number_input("Berapa besar negara (Bagian C)?", min_value=1, max_value=None)
-
-df = df_
-dfJ = df_info
-
-kode_negara = df['kode_negara'].tolist()
-kode_negara = list(dict.fromkeys(kode_negara))
-
-produksi_total = []
-negara_ = []
-
-for kode in kode_negara:
-    try:
-        produksi = df[df['kode_negara']==kode]['produksi'].tolist()
-        negara = dfJ[dfJ['alpha-3']==kode]['name'].tolist()[0]
-        produksi_total.append(np.sum(np.array(produksi)))
-        negara_.append(negara)
-    except:
-        continue
+for i in list (dfC['kode_negara']) :
+    if i not in list_a:
+        list_a.append(i)
         
-dic = {'negara':negara_,'produksi_total':produksi_total}
-df__ = pd.DataFrame(dic)
-df__ = df__.sort_values('produksi_total',ascending=False).reset_index()
+for i in list_a :
+    a=dfC.loc[dfC['kode_negara'] ==i,'produksi'].sum()
+    kumulatif.append(a)
+    
+dk = pd.DataFrame(list(zip(list_a,kumulatif)), columns = ['kode_negara','kumulatif'])
+dk = dk.sort_values(by=['kumulatif'], ascending = False)
+dk = dk[:B]
 
-plt.clf() # clear the figure
-
-#tulisan nanti lu aja ya, gua update ke github dulu
-
-plt.title('{B} Negara dengan Produksi Terbesar Kumulatif'.format(B=B_))
-plt.bar(df__['negara'][:B_],df__['produksi_total'][:B_],width=0.9, bottom=None, align="center",
-            color="lightblue", edgecolor="aquamarine", data=None, zorder=3)
-plt.grid(True, color="grey", linewidth="0.7", linestyle="-.", zorder=0)
-plt.xlabel('negara')
-plt.ylabel('produksi_total')
-
-st.write('Input banyak negara')
+dk.plot.bar(x='kode_negara', y='kumulatif') 
+plt.show()
 st.pyplot(plt)
 
-#--Poin (d)--
-st.write()
-st.write()
-st.header('Bagian D COKK')
+#Bagian D
 
-T_ = st.sidebar.number_input("Summary Tahun Produksi", min_value=1971, max_value=2015)
+T = st.sidebar.number_input("Pilih Tahun produksi", min_value=1971, max_value=2015)
 
-df = ch_.dataFrame
-dfJ = jh_.dataFrame
-
-tahun = list(dict.fromkeys(df['tahun'].tolist()))
+tahun = list(dict.fromkeys(dfC['tahun'].tolist()))
 
 dic_maks = {'negara':[],
             'kode_negara':[],
@@ -181,13 +119,13 @@ dic_zero = {'negara':[],
             'tahun':tahun}
 
 for t in tahun:
-    df_per_tahun = df[df['tahun']==t]
-    produksi = np.array(df_per_tahun['produksi'].tolist())
+    dfC_per_tahun = dfC[dfC['tahun']==t]
+    produksi = np.array(dfC_per_tahun['produksi'].tolist())
     maks_prod = max(produksi)
     min_prod = min([p for p in produksi if p != 0])
     zero_prod = min([p for p in produksi if p == 0])
     # maksimum
-    kode_negara = df_per_tahun[df_per_tahun['produksi']==maks_prod]['kode_negara'].tolist()[0]
+    kode_negara = dfC_per_tahun[dfC_per_tahun['produksi']==maks_prod]['kode_negara'].tolist()[0]
     if kode_negara == 'WLD':
         kode_negara = 'WLF'
     dic_maks['negara'].append(dfJ[dfJ['alpha-3']==kode_negara]['name'].tolist()[0])
@@ -196,7 +134,7 @@ for t in tahun:
     dic_maks['sub_region'].append(dfJ[dfJ['alpha-3']==kode_negara]['sub-region'].tolist()[0])
     dic_maks['produksi'].append(maks_prod)
     # minimum != 0
-    kode_negara = df_per_tahun[df_per_tahun['produksi']==min_prod]['kode_negara'].tolist()[0]
+    kode_negara = dfC_per_tahun[dfC_per_tahun['produksi']==min_prod]['kode_negara'].tolist()[0]
     if kode_negara == 'WLD':
         kode_negara = 'WLF'
     dic_min['negara'].append(dfJ[dfJ['alpha-3']==kode_negara]['name'].tolist()[0])
@@ -205,7 +143,7 @@ for t in tahun:
     dic_min['sub_region'].append(dfJ[dfJ['alpha-3']==kode_negara]['sub-region'].tolist()[0])
     dic_min['produksi'].append(min_prod)
     # zero == 0
-    kode_negara = df_per_tahun[df_per_tahun['produksi']==zero_prod]['kode_negara'].tolist()[0]
+    kode_negara = dfC_per_tahun[dfC_per_tahun['produksi']==zero_prod]['kode_negara'].tolist()[0]
     if kode_negara == 'WLD':
         kode_negara = 'WLF'
     dic_zero['negara'].append(dfJ[dfJ['alpha-3']==kode_negara]['name'].tolist()[0])
@@ -217,21 +155,3 @@ for t in tahun:
 df_maks = pd.DataFrame(dic_maks)
 df_min = pd.DataFrame(dic_min)
 df_zero = pd.DataFrame(dic_zero)
-
-st.write('Info Produksi Maksimum Tahun ke-{}'.format(T_))
-st.write(df_maks[df_maks['tahun']==T_])
-
-st.write('Tabel Maks per Tahun')
-st.write(df_maks)
-
-st.write('Info Produksi Minimum (Not Zero) Tahun ke-{}'.format(T_))
-st.write(df_min[df_min['tahun']==T_])
-
-st.write('Tabel Min (Not Zero) per Tahun')
-st.write(df_min)
-
-st.write('Info Produksi Zero Tahun ke-{}'.format(T_))
-st.write(df_zero[df_zero['tahun']==T_])
-
-st.write('Tabel Zero per Tahun')
-st.write(df_zero)
